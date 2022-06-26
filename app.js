5b7a4ddc3011743c1418e39c321bd7a83d895e71
Daniel Mobbs <dan@danmobbs.co>
2022-06-26 12:54:46 +0100

    Added a Save function, but need to fix the overflow issues when saving the previous form

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const results = document.getElementById("results");
-const addBtn = document.getElementById("add");
-const resetBtn = document.getElementById("reset");
+const addBtn = document.getElementById("add--btn");
+const resetBtn = document.getElementById("reset--btn");
+const saveBtn = document.getElementById("save--btn");
+const savedResults = document.getElementById("saved__results");
 
 let count = 0;
 
 addBtn.addEventListener("click", () => {
-    count = count + 1;
+    count++;
     results.innerText = count;
 })
 
-resetBtn.addEventListener("click", () => {
+function save() {
+    savedResults.style.display = "block";
+    let countStr = count + " - ";
+    savedResults.innerText += countStr;
+}
+
+saveBtn.addEventListener( "click", save );
+
+// Resets the form
+resetBtn.addEventListener( "click", () => {
     count = 0;
+    countStr = '';
+    savedResults.style.display = 'none';    
     results.innerText = 0;
 })
